618cc4b5bf3989ee89783b5993baf007ce415e6b
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-09-14 12:29:34 +0800

    upload float/double test

---
+++ programming_class/float_double_test.cpp
@@ -0,0 +1,9 @@
+#include <iomanip>
+#include <iostream>
+using namespace std;
+int main(){
+    float y1 = 1/3.;
+    cout<<setprecision(30)<<y1<<endl;
+    double y2 = 1/3.;
+    cout<<setprecision(30)<<y2<<endl;
+}
