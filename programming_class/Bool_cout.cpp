5ee3ce30cd81b9f397436ff62a5797a8e497637f
Zhe Wei Chang <126851525+Liyue-Wei@users.noreply.github.com>
2024-09-23 00:54:56 +0800

    0922

--- programming_class/Bool_cout.cpp
+++ programming_class/Bool_cout.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main(){
     bool flag = true;
     cout.setf(ios::boolalpha);
-    cout << flag;
-    cout << static_cast<bool>(1);
+    cout << flag << "\n";
+    cout << static_cast<bool>(1) << "\n";
     flag = false;
-    cout << flag;
-    cout << static_cast<bool>(0);
+    cout << flag << "\n";
+    cout << static_cast<bool>(0) << "\n";
 }
--- programming_class/float_double_test.cpp
+++ programming_class/float_double_test.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main(){
     float y1 = 1/3.;
-    cout<<setprecision(30)<<y1<<endl;
+    cout << setprecision(32) << y1<<endl;
     double y2 = 1/3.;
-    cout<<setprecision(30)<<y2<<endl;
+    cout << setprecision(32) << y2<<endl;
 }
